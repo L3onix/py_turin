8183eb508a5ef6ff9949452c73c0bb415f120e90
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2019-05-21 18:09:54 -0300

    primeiro commit

---
+++ maquina.py
@@ -0,0 +1,26 @@
+
+class Maquina:
+    estado = ''
+    leitor = ''
+
+    
+    def consultar_tabela_de_processamento():
+
+        if (estado = 'INICIO'):
+            if (celula = '*'):
+                pass
+            pass
+        elif (estado = 'ADIÇÃO'):
+            if (celula = '0'):
+                pass
+            elif (celula = '1'):
+                pass
+            elif (celula = '*'):
+                pass
+            pass
+        elif (estado = 'TRANSPORTE'):
+            pass
+        elif (estado = 'TRANSBORDAMENTO'):
+            pass
+        elif (estado = 'RETORNO'):
+            pass
