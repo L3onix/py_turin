24bf8f212b92cf83bfe53ef3446f45675b1a972e
L3onix Leonardo Facundes <leonardo.fm00@gmail.com>
2019-05-29 01:55:30 -0300

    finalizado

--- maquina.py
+++ maquina.py
@@ -1,26 +1,116 @@
 
 class Maquina:
-    estado = ''
-    leitor = ''
+    estado = 'VAZIO'
+    celula = 'VAZIO'
 
-    
-    def consultar_tabela_de_processamento():
+    # Sempre retorna a direção em que o leitor de se mover
+    def consultar_tabela_de_processamento(self, leitura):
+        self.celula = leitura
+        if (self.estado == 'INICIO'):
+            if (self.celula == '*'):
+                self.estado = 'ADIÇÃO'
 
-        if (estado = 'INICIO'):
-            if (celula = '*'):
-                pass
+                resultados = {
+                    'direcao': 'ESQUERDA',
+                    'valor_a_escrever': '*'
+                }
+                return resultados
+            
             pass
-        elif (estado = 'ADIÇÃO'):
-            if (celula = '0'):
-                pass
-            elif (celula = '1'):
-                pass
-            elif (celula = '*'):
-                pass
+
+        elif (self.estado == 'ADIÇÃO'):
+            if (self.celula == '0'):
+                self.estado = 'RETORNO'
+
+                resultados = {
+                    'direcao': 'DIREITA',
+                    'valor_a_escrever': '1'
+                }
+                return resultados
+
+            elif (self.celula == '1'):
+                self.estado = 'TRANSPORTE'
+
+                resultados = {
+                    'direcao': 'ESQUERDA',
+                    'valor_a_escrever': '0'
+                }
+                return resultados
+                
+            elif (self.celula == '*'):
+                self.estado = 'PARAR'
+
+                resultados = {
+                    'direcao': 'DIREITA',
+                    'valor_a_escrever': '*'
+                }
+                return resultados
+
             pass
-        elif (estado = 'TRANSPORTE'):
+            
+        elif (self.estado == 'TRANSPORTE'):
+            if (self.celula == '0'):
+                self.estado = 'RETORNO'
+
+                resultados = {
+                    'direcao': 'DIREITA',
+                    'valor_a_escrever': '1'
+                }
+                return resultados
+
+            elif (self.celula == '1'):
+                self.estado = 'TRANSPORTE'
+
+                resultados = {
+                    'direcao': 'ESQUERDA',
+                    'valor_a_escrever': '0'
+                }
+                return resultados
+                
+            elif (self.celula == '*'):
+                self.estado = 'TRANSBORDAMENTO'
+
+                resultados = {
+                    'direcao': 'ESQUERDA',
+                    'valor_a_escrever': '1'
+                }
+                return resultados
+
+
+        elif (self.estado == 'TRANSBORDAMENTO'):
+            if (self.celula == '-'):
+                self.estado = 'RETORNO'
+
+                resultados = {
+                    'direcao': 'DIREITA',
+                    'valor_a_escrever': '*'
+                }
+                return resultados
             pass
-        elif (estado = 'TRANSBORDAMENTO'):
+        elif (self.estado == 'RETORNO'):
+            if (self.celula == '0'):
+                self.estado = 'RETORNO'
+
+                resultados = {
+                    'direcao': 'DIREITA',
+                    'valor_a_escrever': '0'
+                }
+                return resultados
+            if (self.celula == '1'):
+                self.estado = 'RETORNO'
+
+                resultados = {
+                    'direcao': 'DIREITA',
+                    'valor_a_escrever': '1'
+                }
+                return resultados
+            if (self.celula == '*'):
+                self.estado = 'PARAR'
+
+                resultados = {
+                    'direcao': 'NAO_SE_MOVA',
+                    'valor_a_escrever': '*'
+                }
+                return resultados
             pass
-        elif (estado = 'RETORNO'):
-            pass
+
--- principal.py
+++ principal.py
@@ -1,1 +1,74 @@
+from maquina import Maquina
+from os import system
+from time import sleep
+
+#transforma a entrada do usuário em uma fita de processamento
+def frase_to_fita(frase):
+    fita = ['-', '*']
+    for x in frase:
+        fita.append(x)
+    fita.append('*')
+    return fita
+
+def processar_fita(fita, maquina):
+    pos_inicial = len(fita) - 1
+    pos_atual = pos_inicial
+    pos_final = 0
+
+    maquina.estado = 'INICIO'
+
+    while not(maquina.estado == 'PARAR'):
+        resultado_processamento = maquina.consultar_tabela_de_processamento(fita[pos_atual])
+
+        #imprimindo antes de reescrever celula
+        imprimir_tela(fita.copy(), pos_atual, maquina.estado, 
+            resultado_processamento['direcao'], resultado_processamento['valor_a_escrever'])
+        input()
+        #enter para apresentar próximo passo, se quiser automático substitua por print()
+
+        fita[pos_atual] = resultado_processamento['valor_a_escrever']
+
+        #imprime depois de reescrever celula
+        imprimir_tela(fita.copy(), pos_atual, maquina.estado, 
+            resultado_processamento['direcao'], resultado_processamento['valor_a_escrever'])
+        input()
+
+        pos_atual = ajustar_pos_atual(resultado_processamento['direcao'], pos_atual)
+
+def imprimir_tela(fita, pos_atual, estado, direcao, valor_escrita):
+    system('clear')
+    line = ('__________________________')
+
+    #IMPRIMINDO INFORMAÇÕES DA MÁQUINA
+    maquina_title = line+'>> MAQUINA <<'+line
+    print(maquina_title)
+    print('ESTADO = ' + estado)
+    print('DIREÇÃO = ' + direcao)
+    print('VALOR P/ ESCREVER = ' + valor_escrita)
+    print()
+
+    #IMPRIMINDO FITA
+    fita_title = line+'>> FITA <<'+line
+    fita[pos_atual] = '< '+fita[pos_atual]+' >'
+    print(fita_title)
+    print(fita)
+    
+#ajusta a posição atual de acordo com o comando de direção retornado pela tabela de processamento
+def ajustar_pos_atual(direcao, pos_atual):
+    if (direcao == 'ESQUERDA'):
+        return pos_atual - 1
+    elif (direcao == 'DIREITA'):
+        return pos_atual + 1
+
+
+#criando uma máquina
+mk_1 = Maquina()
+
+#fita a ser processada
+entrada_user = '101'
+fita = frase_to_fita(entrada_user)
+
+#processamento da fita
+system('clear')
+processar_fita(fita, mk_1)
 
